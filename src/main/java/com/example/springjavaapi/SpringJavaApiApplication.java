e10e22a11e7612671d0f73a61332ecd12ddbaaf9
pedroygor <pedroygor2010@hotmail.com>
2023-06-01 16:54:40 -0300

    first steps java

---
+++ src/main/java/com/example/springjavaapi/SpringJavaApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.springjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaApiApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+}
